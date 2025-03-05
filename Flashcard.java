1857b021375c166ae1e33cb69cf9cfc9faa3e72c
Atharv Nakti <128978076+atharv3434@users.noreply.github.com>
2025-03-05 22:59:56 +0530

    Add files via upload

---
+++ Flashcard.java
@@ -0,0 +1,20 @@
+public class Flashcard {
+    private String question;
+    private String answer;
+
+    public Flashcard(String question, String answer) {
+        this.question = question;
+        this.answer = answer;
+    }
+
+    public String getQuestion() { return question; }
+    public String getAnswer() { return answer; }
+
+    public void setQuestion(String question) { this.question = question; }
+    public void setAnswer(String answer) { this.answer = answer; }
+
+    @Override
+    public String toString() {
+        return "📝 Question: " + question + "\n💡 Answer: " + answer;
+    }
+}
